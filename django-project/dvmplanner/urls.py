57aaec052ae8dba1ddf878332dc839891d535634
Kai Singer <kai.singer@outlook.de>
2024-11-20 15:30:54 +0100

    adding pages

--- django-project/dvmplanner/urls.py
+++ django-project/dvmplanner/urls.py
@@ -2,5 +2,11 @@ from django.urls import path
 from dvmplanner import views
 
 urlpatterns = [
-  path('', views.dashboard, name = 'dashboard')
+  path('', views.home, name = 'home'),
+  path('dashboard/', views.dashboard, name = 'dashboard'),
+  path('reports/', views.reports, name = 'reports'),
+  path('review/', views.review, name = 'review'),
+  path('admin/', views.admin, name = 'admin'),
+  path('profile/', views.profile, name = 'profile'),
+  path('addreport/', views.addreport, name = 'addreport'),
 ]
--- django-project/dvmplanner/views.py
+++ django-project/dvmplanner/views.py
@@ -1,5 +1,23 @@
 from django.shortcuts import redirect, render
 from django.core.files.storage import FileSystemStorage
 
+def home(request):
+  return redirect(dashboard)
+
 def dashboard(request):
   return render(request, 'dvmplanner/dashboard.html')
+
+def reports(request):
+  return render(request, 'dvmplanner/reports.html')
+
+def review(request):
+  return render(request, 'dvmplanner/review.html')
+
+def admin(request):
+  return render(request, 'dvmplanner/admin.html')
+
+def profile(request):
+  return render(request, 'dvmplanner/profile.html')
+
+def addreport(request):
+  return render(request, 'dvmplanner/addreport.html')
