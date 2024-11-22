92221eb0ea038ccc6ce319f179309d62fc4bf58c
Kai Singer <kai.singer@outlook.de>
2024-11-22 12:11:43 +0100

    adding close menu icon

--- django-project/dvmplanner/static/scripts/main.js
+++ django-project/dvmplanner/static/scripts/main.js
@@ -1,6 +1,18 @@
 function toggleNavbar() {
   const navbar = document.getElementById('navbar');
   const content = document.getElementById('content');
-  navbar.classList.toggle('shown_navbar');
-  content.classList.toggle('hidden_content');
+  const img = document.getElementById('navbar_hide_button_img');
+
+  if (navbar.classList.contains('shown_navbar')) {
+    navbar.classList.remove('shown_navbar');
+    content.classList.remove('hidden_content');
+    img.src = staticMenuIcon;
+    img.alt = 'Open Menu';
+
+  } else {
+    navbar.classList.add('shown_navbar');
+    content.classList.add('hidden_content');
+    img.src = staticCloseMenuIcon;
+    img.alt = 'Close Menu';
+  }
 }
