b42716609bd592769487b6d924a861619aeef5e7
Kai Singer <kai.singer@outlook.de>
2024-11-22 17:00:40 +0100

    added topbar to all sites

--- django-project/dvmplanner/views.py
+++ django-project/dvmplanner/views.py
@@ -1,31 +1,43 @@
 from django.shortcuts import redirect, render
 from django.core.files.storage import FileSystemStorage
+from django.contrib.staticfiles import finders
 from dvmplanner.scripts.users import User
+import os
+
+uid = 'u0000'
+if not finders.find(f'profiles/{uid}.png'):
+  uid = '_default'
+data = {
+  'uid': uid,
+  'username': 'testuser',
+  'first_name': 'Max',
+  'last_name': 'Mustermann'
+}
 
 def home(request):
   return redirect(dashboard)
 
 def dashboard(request):
-  usernames = []
-  users = User.getUsers()
-  for user in users:
-    usernames.append(user.getUsername())
-    data = {
-      'usernames': usernames
-    }
+  # usernames = []
+  # users = User.getUsers()
+  # for user in users:
+  #   usernames.append(user.getUsername())
+  # data = {
+  #   'usernames': usernames
+  # }
   return render(request, 'dvmplanner/dashboard.html', data)
 
 def reports(request):
-  return render(request, 'dvmplanner/reports.html')
+  return render(request, 'dvmplanner/reports.html', data)
 
 def review(request):
-  return render(request, 'dvmplanner/review.html')
+  return render(request, 'dvmplanner/review.html', data)
 
 def admin(request):
-  return render(request, 'dvmplanner/admin.html')
+  return render(request, 'dvmplanner/admin.html', data)
 
 def profile(request):
-  return render(request, 'dvmplanner/profile.html')
+  return render(request, 'dvmplanner/profile.html', data)
 
 def addreport(request):
-  return render(request, 'dvmplanner/addreport.html')
+  return render(request, 'dvmplanner/addreport.html', data)
