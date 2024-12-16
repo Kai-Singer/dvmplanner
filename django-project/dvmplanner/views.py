36b2b4e2c1d04be9a5e2e56a09700d33dc2f0339
Kai Singer <kai.singer@outlook.de>
2024-12-16 22:54:52 +0100

    Admin HTML completed

--- django-project/dvmplanner/views.py
+++ django-project/dvmplanner/views.py
@@ -495,11 +495,7 @@ def admin(request):
     'first_name': 'Max',
     'last_name': 'Mustermann',
     'role': 'admin',
-    'current_filter_role': '[Alle]',
-    'filter_roles': [
-      'VIP',
-      'Admin'
-    ],
+    'current_requests_filter_role': '[Alle]',
     'requests': [
       {
         'day': '15.12.2024',
@@ -528,6 +524,497 @@ def admin(request):
         'role': 'vip',
         'requested_role': 'admin',
       },
+    ],
+    'current_users_filter_role': '[Alle]',
+    'current_users_filter_status': '[Alle]',
+    'users': [
+      {
+        'day': '15.12.2024',
+        'time': '10.12:43 Uhr',
+        'first_name': 'Julius',
+        'last_name': 'Cäsar',
+        'username': 'thedestroyer123',
+        'role': 'vip',
+        'status': 'active',
+      },
+      {
+        'day': '13.12.2024',
+        'time': '15.12:43 Uhr',
+        'first_name': 'Leonardo',
+        'last_name': 'da Vinci',
+        'username': 'the_real_vinci',
+        'role': 'normal',
+        'status': 'inactive',
+      },
+      {
+        'day': '12.12.2024',
+        'time': '13.12:43 Uhr',
+        'first_name': 'Marie',
+        'last_name': 'Curie',
+        'username': 'radium',
+        'role': 'vip',
+        'status': 'blocked',
+      },
+      {
+        'day': '10.12.2024',
+        'time': '12.12:43 Uhr',
+        'first_name': 'Bernd',
+        'last_name': 'Brot',
+        'username': 'bernddasbrot',
+        'role': 'admin',
+        'status': 'deleted',
+      },
+    ],
+    'modules': [
+      {
+        "index": "1",
+        "name": "Technische Dimensionen der Digitalisierung",
+        "modules": [
+          {
+            "index": "1.1",
+            "name": "Informatik Einführung und Vertiefung",
+            "submodules": [
+              {
+                "index": "1.1.1",
+                "name": "Einführung in die Informatik",
+                "semester": "2"
+              },
+              {
+                "index": "1.1.2",
+                "name": "Vertiefung Informatik",
+                "semester": "3"
+              }
+            ]
+          },
+          {
+            "index": "1.2",
+            "name": "E-Government mit Exkursionen",
+            "submodules": [
+              {
+                "index": "1.2.0",
+                "name": "E-Government mit Exkursionen",
+                "semester": "1"
+              }
+            ]
+          },
+          {
+            "index": "1.3",
+            "name": "Betriebs- und Kommunikationssysteme/ verteilte Systeme",
+            "submodules": [
+              {
+                "index": "1.3.1",
+                "name": "Betriebs- und Kommunikationssysteme",
+                "semester": "3"
+              },
+              {
+                "index": "1.3.2",
+                "name": "Verteilte Systeme",
+                "semester": "4"
+              },
+              {
+                "index": "1.3.3",
+                "name": "Aktuelle Anwendungen erproben, Web Collagen, Roboter bauen, Raspberry Pi",
+                "semester": "4"
+              }
+            ]
+          },
+          {
+            "index": "1.4",
+            "name": "Vorgehensmodelle der Softwareentwicklung, Requirements-Engineering und Requirements-Management",
+            "submodules": [
+              {
+                "index": "1.4.1",
+                "name": "Vorgehensmodelle (Software-Engineering 1)",
+                "semester": "1"
+              },
+              {
+                "index": "1.4.2",
+                "name": "Requirements-Engineering und Requirements-Management (Software-Engineering 2)",
+                "semester": "2"
+              }
+            ]
+          },
+          {
+            "index": "1.5",
+            "name": "Systemanalyse, Softwareentwurf und Implementierung, Softwarequilität und Test",
+            "submodules": [
+              {
+                "index": "1.5.1",
+                "name": "Systemanalyse (Software-Engineering 3)",
+                "semester": "3"
+              },
+              {
+                "index": "1.5.2",
+                "name": "Softwareentwurf und Implementierung (Software-Engineering 4)",
+                "semester": "4"
+              },
+              {
+                "index": "1.5.3",
+                "name": "Softwarequalität und Test (Software-Engineering 5)",
+                "semester": "4"
+              }
+            ]
+          },
+          {
+            "index": "1.6",
+            "name": "Cybersecurity und ITIL",
+            "submodules": [
+              {
+                "index": "1.6.1",
+                "name": "Cybersecurity",
+                "semester": "6"
+              },
+              {
+                "index": "1.6.2",
+                "name": "ITIL",
+                "semester": "6"
+              }
+            ]
+          },
+          {
+            "index": "1.7",
+            "name": "IT-Management",
+            "submodules": [
+              {
+                "index": "1.7.0",
+                "name": "IT-Management",
+                "semester": "5"
+              }
+            ]
+          },
+          {
+            "index": "1.8",
+            "name": "IT-Systeme und Informationssysteme",
+            "submodules": [
+              {
+                "index": "1.8.0",
+                "name": "IT-Systeme und Informationssysteme",
+                "semester": "5"
+              }
+            ]
+          }
+        ]
+      },
+      {
+        "index": "2",
+        "name": "Verwaltungsmanagement",
+        "modules": [
+          {
+            "index": "2.1",
+            "name": "Steuerung, Public Management und Projektmanagement",
+            "submodules": [
+              {
+                "index": "2.1.0",
+                "name": "Steuerung, Public Management und Projektmanagement",
+                "semester": "1"
+              }
+            ]
+          },
+          {
+            "index": "2.2",
+            "name": "Organisations- und Prozessmanagement",
+            "submodules": [
+              {
+                "index": "2.2.0",
+                "name": "Organisations- und Prozessmanagement",
+                "semester": "3"
+              }
+            ]
+          },
+          {
+            "index": "2.3",
+            "name": "Öffentliche Betriebswirtschaftslehre",
+            "submodules": [
+              {
+                "index": "2.3.0",
+                "name": "Öffentliche Betriebswirtschaftslehre",
+                "semester": "5"
+              }
+            ]
+          }
+        ]
+      },
+      {
+        "index": "3",
+        "name": "Rechtliche Grundlagen der öffentlichen Verwaltung",
+        "modules": [
+          {
+            "index": "3.1",
+            "name": "Öffentlich-rechtliche Grundlagen der Verwaltungsorganisation und des Verwaltungshandelns",
+            "submodules": [
+              {
+                "index": "3.1.1",
+                "name": "Grundlagen des Staats- und Europarechts",
+                "semester": "1"
+              },
+              {
+                "index": "3.1.2",
+                "name": "Verwaltungsrecht",
+                "semester": "1"
+              }
+            ]
+          },
+          {
+            "index": "3.2",
+            "name": "Kommunales Wirtschaftsrecht",
+            "submodules": [
+              {
+                "index": "3.2.1",
+                "name": "Kommunalrecht",
+                "semester": "3"
+              },
+              {
+                "index": "3.2.2",
+                "name": "Finanzwirtschaft der Kommunen",
+                "semester": "3"
+              },
+              {
+                "index": "3.2.3",
+                "name": "Staatliches Haushaltsrecht",
+                "semester": "3"
+              }
+            ]
+          },
+          {
+            "index": "3.3",
+            "name": "Zivilrechtliche Grundlagen des Verwaltungshandelns",
+            "submodules": [
+              {
+                "index": "3.3.1",
+                "name": "Grundlagen des Zivilrechts",
+                "semester": "2"
+              },
+              {
+                "index": "3.3.2",
+                "name": "Grundlagen des Kartell- und Wettbewerbsrechts",
+                "semester": "2"
+              },
+              {
+                "index": "3.3.3",
+                "name": "Grundlagen des Handels- und Gesellschaftsrechts",
+                "semester": "2"
+              },
+              {
+                "index": "3.3.4",
+                "name": "IT-Recht",
+                "semester": "2"
+              }
+            ]
+          },
+          {
+            "index": "3.4",
+            "name": "Rechtliche Grundlagen der öffentlichen Beschaffung",
+            "submodules": [
+              {
+                "index": "3.4.1",
+                "name": "Vergaberecht (einschließlich E-Government)",
+                "semester": "4"
+              },
+              {
+                "index": "3.4.2",
+                "name": "Beihilferecht",
+                "semester": "4"
+              }
+            ]
+          },
+          {
+            "index": "3.5",
+            "name": "Rechtliche Grundlagen des Datenschutzes, Informationszugangsrecht und Personalrecht",
+            "submodules": [
+              {
+                "index": "3.5.1",
+                "name": "Recht Datenschutzes",
+                "semester": "5"
+              },
+              {
+                "index": "3.5.2",
+                "name": "Informationszugangsrecht",
+                "semester": "5"
+              },
+              {
+                "index": "3.5.3",
+                "name": "Arbeitsrecht",
+                "semester": "5"
+              },
+              {
+                "index": "3.5.4",
+                "name": "Beamtenrecht",
+                "semester": "5"
+              }
+            ]
+          },
+          {
+            "index": "3.6",
+            "name": "Vertragsgestaltung und rechtliche Kernkompetenzen",
+            "submodules": [
+              {
+                "index": "3.6.1",
+                "name": "Vertragsgestaltung",
+                "semester": "6"
+              },
+              {
+                "index": "3.6.2",
+                "name": "Rechtliche Kernkompetenzen bei Digitalisierungsprojekten",
+                "semester": "6"
+              }
+            ]
+          }
+        ]
+      },
+      {
+        "index": "4",
+        "name": "Digital Leadership",
+        "modules": [
+          {
+            "index": "4.1",
+            "name": "Digital Governance: Von der ganzheitlichen Strategie zur Umsetzung",
+            "submodules": [
+              {
+                "index": "4.1.1",
+                "name": "Strategische und integrale Steuerung",
+                "semester": "1"
+              },
+              {
+                "index": "4.1.2",
+                "name": "Smart Cities und Smart Services",
+                "semester": "1"
+              },
+              {
+                "index": "4.1.3",
+                "name": "Grundlagen des Change Managements",
+                "semester": "2"
+              }
+            ]
+          },
+          {
+            "index": "4.2",
+            "name": "Führung, Kommunikation und Partizipation im digitalen Kontext",
+            "submodules": [
+              {
+                "index": "4.2.1",
+                "name": "Gestaltung von Kommunikation und Partizipation mit digitalen Medien",
+                "semester": "4"
+              },
+              {
+                "index": "4.2.2",
+                "name": "Führung und Teamentwicklung mit digitalen Medien",
+                "semester": "3"
+              },
+              {
+                "index": "4.2.3",
+                "name": "Digitalisierung und digitales Wissensmanagement",
+                "semester": "4"
+              }
+            ]
+          }
+        ]
+      },
+      {
+        "index": "5",
+        "name": "Praxisphasen Praktika 1 und 7",
+        "modules": [
+          {
+            "index": "5.1",
+            "name": "Praktikum 1",
+            "submodules": [
+              {
+                "index": "5.1.0",
+                "name": "Praktikum 1",
+                "semester": "1"
+              }
+            ]
+          },
+          {
+            "index": "5.2",
+            "name": "Praktikum 7",
+            "submodules": [
+              {
+                "index": "5.2.0",
+                "name": "Praktikum 7",
+                "semester": "6"
+              }
+            ]
+          }
+        ]
+      },
+      {
+        "index": "6",
+        "name": "Fallstudien",
+        "modules": [
+          {
+            "index": "6.1",
+            "name": "Fallstudie 1",
+            "submodules": [
+              {
+                "index": "6.1.0",
+                "name": "Fallstudie 1",
+                "semester": "2"
+              }
+            ]
+          },
+          {
+            "index": "6.2",
+            "name": "Fallstudie 2",
+            "submodules": [
+              {
+                "index": "6.2.0",
+                "name": "Fallstudie 2",
+                "semester": "3"
+              }
+            ]
+          },
+          {
+            "index": "6.3",
+            "name": "Fallstudie 3",
+            "submodules": [
+              {
+                "index": "6.3.0",
+                "name": "Fallstudie 3",
+                "semester": "4"
+              }
+            ]
+          },
+          {
+            "index": "6.4",
+            "name": "Fallstudie 4",
+            "submodules": [
+              {
+                "index": "6.4.0",
+                "name": "Fallstudie 4",
+                "semester": "5"
+              }
+            ]
+          },
+          {
+            "index": "6.5",
+            "name": "Fallstudie 5",
+            "submodules": [
+              {
+                "index": "6.5.0",
+                "name": "Fallstudie 5",
+                "semester": "6"
+              }
+            ]
+          }
+        ]
+      },
+      {
+        "index": "7",
+        "name": "Bachelorarbeit",
+        "modules": [
+          {
+            "index": "7.0",
+            "name": "Bachelorarbeit",
+            "submodules": [
+              {
+                "index": "7.0.0",
+                "name": "Bachelorarbeit",
+                "semester": "6"
+              }
+            ]
+          }
+        ]
+      }
     ]
   }
   return render(request, 'dvmplanner/admin.html', data)
